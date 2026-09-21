46c6a4e55feee5637d9d276971d2c8697a19bfad
FaizMuhammadRamadhan <faiz.m3110@gmail.com>
2026-09-21 11:26:01 +0700

    optimize perform from 89

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('remixicon')) {
+            return 'remixicon';
+          }
+          if (id.includes('node_modules/framer-motion') || id.includes('node_modules/react-router-dom')) {
+            return 'vendor-ui';
+          }
+        },
+      },
+    },
+  },
 })
